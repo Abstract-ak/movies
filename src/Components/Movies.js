1c03081700584442e9f9047575d8673357c084e8
Abstract-ak <1906308@kiit.ac.in>
2023-03-18 14:43:22 +0530

    next and previous page handle

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,31 +1,73 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
-import axios from 'axios';
+// import { movies } from "./getMovies";
+import axios from "axios";
 export default class Movies extends Component {
-    constructor(){
-        super();
-        //used for making states
-        this.state={
-            hover:'',
-            parr:[1],
-            currPage:1
+  constructor() {
+    super();
+    //used for making states
+    this.state = {
+      hover: "",
+      parr: [1],
+      currPage: 1,
+      movies: [],
+    };
+  }
+  async componentDidMount() {
+    //side effects waale kaam jisme tym lagne waala hai
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=e119d74c7ded1379bd00c9a22895630a&language=en-US&page=${this.state.currPage}`
+    );
+    let data = res.data;
+    // console.log(data);
+    this.setState({ movies: [...data.results] });
+    console.log("mounting done");
+  }
+
+  changeMovies=async()=>{
+    console.log("changemovies called");
+    console.log(this.state.currPage);
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+    let data = res.data
+    // console.log(data);
+    this.setState({
+        movies:[...data.results] });
+  }
+
+  handleRight=()=>{
+    let temparr =[]
+    for(let i=1;i<=this.state.parr.length+1;i++){
+        temparr.push(i);
+    }
+    //setstate is async. so after calling setstate we have to give a callback fnctn.
+    this.setState({
+        parr:[...temparr],
+        currPage:this.state.currPage+1
+    },this.changeMovies)
+    }
+
+    handleLeft=()=>{if(this.state.currPage!=1){
+        this.setState({
+            currPage:this.state.currPage-1
+        },this.changeMovies)
+    }}
+
+    handleClick=(value)=>{
+        if(value!=this.state.currPage){
+            console.log("i am called")
+            this.setState({
+                currPage:value
+            },this.changeMovies)
         }
     }
-    async componentDidMount(){
-        //side effects waale kaam jisme tym lagne waala hai
-        const res= await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=e119d74c7ded1379bd00c9a22895630a&language=en-US&page=${this.state.currPage}`);
-        let data= res.data;
-        console.log(data);
-        console.log('mounting done');
-    }
+
   render() {
-    console.log('called render');
-    let movie = movies.results;
+    console.log("render");
+    // let movie = movies.results;
     return (
       <>
         {
           //ternary operator
-          movie.length == 0 ? (
+          this.state.movies.length == 0 ? (
             <div class="spinner-border text-primary" role="status">
               <span class="visually-hidden">Loading...</span>
             </div>
@@ -36,9 +78,13 @@ export default class Movies extends Component {
               </h3>
               <div className="movies-list">
                 {/* jsx are written inside{} */}
-                {movie.map((movieobj) => (
-                    // event lagate hai onmouseEnter
-                  <div className="card movies-card" onMouseEnter={()=>this.setState({hover: movieobj.id})} onMouseLeave={()=>this.setState({hover: ''})}>
+                {this.state.movies.map((movieobj) => (
+                  // event lagate hai onmouseEnter
+                  <div
+                    className="card movies-card"
+                    onMouseEnter={() => this.setState({ hover: movieobj.id })}
+                    onMouseLeave={() => this.setState({ hover: "" })}
+                  >
                     <img
                       src={`https://image.tmdb.org/t/p/original${movieobj.backdrop_path}`}
                       alt={movieobj.title}
@@ -56,27 +102,36 @@ export default class Movies extends Component {
                         justifyContent: "center",
                       }}
                     >
-                        {
-                            this.state.hover == movieobj.id && <a className="btn btn-primary movies-button">
-                            Add to Favourite
-                          </a>
-                        }
-                      
+                      {this.state.hover == movieobj.id && (
+                        <a className="btn btn-primary movies-button">
+                          Add to Favourite
+                        </a>
+                      )}
                     </div>
                   </div>
                 ))}
               </div>
-              <div style={{display:'flex',justifyContent:'center'}}>
-              <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                    {
-                        this.state.parr.map((value)=>(
-                            <li class="page-item"><a class="page-link" href="#">{value}</a></li>
-                        ))
-                    }
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                </ul>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <nav aria-label="Page navigation example">
+                  <ul class="pagination">
+                    <li class="page-item">
+                      <a class="page-link" onClick={this.handleLeft}>
+                        Previous
+                      </a>
+                    </li>
+                    {this.state.parr.map((value) => (
+                      <li class="page-item">
+                        <a class="page-link" onClick={this.handleClick}>
+                          {value}
+                        </a>
+                      </li>
+                    ))}
+                    <li class="page-item">
+                      <a class="page-link" onClick={this.handleRight}>
+                        Next
+                      </a>
+                    </li>
+                  </ul>
                 </nav>
               </div>
             </div>
