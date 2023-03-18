b642fe0a376680aad628a995a3c0d056828f0ebe
Abstract-ak <1906308@kiit.ac.in>
2023-03-19 02:56:09 +0530

    column obects

--- src/Components/Favourite.js
+++ src/Components/Favourite.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
-
+import { movies } from './getMovies'
 export default class Favourite extends Component {
   render() {
+    const movie= movies.results;
+    console.log(movie);
+
+    let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
+    27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
+    
     return (
+        
       <div>
         <>
         <div className='main'>
@@ -25,19 +32,23 @@ export default class Favourite extends Component {
                     <table class="table">
                         <thead>
                             <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Popularity</th>
+                            <th scope="col">Rating</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            </tr>
+                            {
+                                movie.map((movieobj)=>(
+                                    <tr>
+                                        <td>{movieobj.original_title}</td>
+                                        <td>{genreids[movieobj.genre_ids[0]]}</td>
+                                        <td>{movieobj.popularity}</td>
+                                        <td>{movieobj.vote_average}</td>
+                                    </tr>
+                                ))
+                            }
                             <tr>
                             <th scope="row">2</th>
                             <td>Jacob</td>
