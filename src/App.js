949e4991677726743a238deda39d4ac164435520
Abstract-ak <1906308@kiit.ac.in>
2023-03-19 00:02:57 +0530

    Favourite page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Navbar from './Components/Navbar';
+import Favourite from './Components/Favourite';
 function App() {
   return (
     <>
     <Navbar/>
-    <Banner/>
-    <Movies/>
+    {/* <Banner/>
+    <Movies/> */}
+    <Favourite/>
     </>
   );
 }
